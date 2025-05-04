1c9e8f09a1ebbbe1cff6e903bcfe11bbd49bba41
Gyles Perez <gylesperez498@gmail.com>
2025-05-04 20:38:14 +0800

    hopefully last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,29 @@
-import Navbar from './components/Navbar.jsx'
+import './App.css'
+import Nav from './components/Nav.jsx'
 import Hero from './components/Hero.jsx'
-import Counter from './components/Counter.jsx'
-import Collaborate from './components/Collaborate.jsx'
-import Users from './components/Users.jsx'
-import HowWork from './components/HowWork.jsx'
-import Benefit from './components/Benefit.jsx'
-import Reviews from './components/Reviews.jsx'
-import Pricing from './components/Pricing.jsx'
-import FAQ from './components/FAQ.jsx'
-import Contact from './components/Contact.jsx'
-import Footer from './components/Footer.jsx'
-import ButtonStart from './components/startbutton.jsx'
+import Team from './components/Team.jsx'
+import Collab from './components/Collab.jsx'
+import Footer from './components/footer/Footer.jsx'
 
 function App() {
+
+
   return (
     <> 
+    <main class="main" id="top" scroll-to-top="scroll-to-top">
+      <div class="content">
+       
+           <Nav/>
 
-      <main className="main" id="top" scroll-to-top="scroll-to-top">
-        <div className="content">
-          <Navbar/>
-          <div data-bs-target="#navbar" data-bs-spy="scroll" tabIndex="0">
-              {/* <Other components/> */}
-              <Hero/>
-              <Counter/>
-              <Users/>
-              <Collaborate/>
-              <HowWork/>
-              <Benefit/>
-              <Reviews/>
-              <Pricing/>
-              <FAQ/>
-              <Contact/>
-          </div>
-          <ButtonStart/>
+       <div data-bs-target="#navbar" data-bs-spy="scroll" tabindex="0">
+          <Hero/>
+          <Team/>
+          <Collab/>
           <Footer/>
-        </div>
-      </main>
+      </div>
+
+     </div>
+    </main>
 
     </>
   )
